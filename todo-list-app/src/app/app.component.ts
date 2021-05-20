cd7d48e8a663def7c80d1ce76118fe74b8368882
Morgan Haskins <morganhaskins@Morgans-MacBook-Pro.local>
2021-05-19 21:58:29 -0400

    Initial project setup

---
+++ todo-list-app/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'todo-list-app';
+  // todo: Todo[] = [
+  //   { task: 'fold clothes', completed: false },
+  //   { task: 'put clothes in dresser', completed: false },
+  //   { task: 'relax', completed: false },
+  //   { task: 'try to pet a cat', completed: true },
+  //   { task: 'pet a dog', completed: false },
+  //   { task: 'be awesome', completed: false },
+  // ];
+
+}
---
+++ todo-list-app/src/app/todo/todo.component.ts
@@ -0,0 +1,29 @@
+import { Component, Input, OnInit } from '@angular/core';
+interface Todo {
+  task: string;
+  completed: boolean;
+}
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css']
+})
+export class TodoComponent implements OnInit {
+
+  
+  todo: Todo[] = [
+    { task: 'fold clothes', completed: false },
+    { task: 'put clothes in dresser', completed: false },
+    { task: 'relax', completed: false },
+    { task: 'try to pet a cat', completed: true },
+    { task: 'pet a dog', completed: false },
+    { task: 'be awesome', completed: false },
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
