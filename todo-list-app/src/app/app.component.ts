c7bdbf2603a1589a3ceae5fd9482f61674a61bb0
Morgan Haskins <morganhaskins@Morgans-MacBook-Pro.local>
2021-05-20 22:00:14 -0400

    Progress so far with styling and functionality. Complete button and x button work with strike through.

--- todo-list-app/src/app/app.component.ts
+++ todo-list-app/src/app/app.component.ts
@@ -8,14 +8,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo-list-app';
-  // todo: Todo[] = [
-  //   { task: 'fold clothes', completed: false },
-  //   { task: 'put clothes in dresser', completed: false },
-  //   { task: 'relax', completed: false },
-  //   { task: 'try to pet a cat', completed: true },
-  //   { task: 'pet a dog', completed: false },
-  //   { task: 'be awesome', completed: false },
-  // ];
+keyword = '';
 
 }
--- todo-list-app/src/app/todo/todo.component.ts
+++ todo-list-app/src/app/todo/todo.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+
 interface Todo {
   task: string;
   completed: boolean;
@@ -10,9 +11,7 @@ interface Todo {
   styleUrls: ['./todo.component.css']
 })
 export class TodoComponent implements OnInit {
-  filter = '';
-
-  
+  keyword: string = "";
   todo: Todo[] = [
     { task: 'fold clothes', completed: false },
     { task: 'put clothes in dresser', completed: false },
@@ -21,6 +20,25 @@ export class TodoComponent implements OnInit {
     { task: 'pet a dog', completed: false },
     { task: 'be awesome', completed: false },
   ];
+
+  styles = {
+    "text-decoration": "line-through"
+  }
+
+  removeTask(index: number){
+    this.todo.splice(index,1)
+  }
+
+  completeTask(index: number){
+    this.todo[index].completed = true;
+  }
+
+  addTask(index: number){
+    this.todo[index].completed = false;
+  }
+
+
+
 
   constructor() { }
 
